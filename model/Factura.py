f5151350f456cc71dc6215c1a70f1b8fe8b751b3
Andres Felipe Grajales <andres.grajales@utp.edu.co>
2022-03-30 23:55:57 -0500

    initial commit

---
+++ model/Factura.py
@@ -0,0 +1,7 @@
+class Factura:
+    def __init__(self) -> None:
+        self.id_factura = self.get_id_nueva_factura()
+        self.list_products
+
+    def get_id_nueva_factura(self):
+        pass
